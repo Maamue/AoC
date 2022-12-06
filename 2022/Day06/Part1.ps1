cf495ab393d9fa105adf0164f8dc70f2768ab383
Maamue <26257726+Maamue@users.noreply.github.com>
2022-12-06 14:33:37 +0100

    Create Part1.ps1

---
+++ 2022/Day06/Part1.ps1
@@ -0,0 +1,17 @@
+$datastream = [System.IO.File]::ReadAllText("$PSScriptRoot\input.txt")
+
+Function Get-MessageMarker {
+    param (
+        [int]
+        $DisinctCharacters
+    )
+
+    for ($i = 0; $i -le $datastream.Length - $DisinctCharacters; $i++) {
+        $identifier = [System.Collections.Generic.HashSet[char]]$datastream[$i..($i + $DisinctCharacters -1)]
+        if ($identifier.Count -eq $DisinctCharacters) { 
+            $i + $DisinctCharacters 
+            break
+        }
+    }
+}
+Get-MessageMarker -DisinctCharacters 4
